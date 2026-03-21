0fc2acdee3810246d099b005f261340afac54a44
DESHAN K.D.M <130528796+DHARAKA-METH@users.noreply.github.com>
2026-03-21 13:43:26 +0530

    implement cores

--- RescuePaws-backend/api-gateway/src/main/java/com/rescuepaws/apigateway/config/CorsConfig.java
+++ RescuePaws-backend/api-gateway/src/main/java/com/rescuepaws/apigateway/config/CorsConfig.java
@@ -1,20 +1,26 @@
 package com.rescuepaws.apigateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
 public class CorsConfig {
+    @Value("${frontend_url}")
+    private String frontendUrl;
+
     @Bean
     public CorsWebFilter corsWebFilter() {
-
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin(frontendUrl);
+        config.addAllowedMethod("*"); // GET, POST, PUT, DELETE, OPTIONS
         config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
         return new CorsWebFilter(source);
